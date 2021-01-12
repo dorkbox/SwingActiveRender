517cf097394f319f5f18ecb66dc6cc8f7f6f5ca7
Robinson <robinson@dorkbox.com>
2021-01-12 21:41:33 +0100

    Moved Swing Active Render module from general Utilities module.

---
+++ README.md
@@ -0,0 +1,39 @@
+Dorkbox General Utilities
+
+###### [![Dorkbox](https://badge.dorkbox.com/dorkbox.svg "Dorkbox")](https://git.dorkbox.com/dorkbox/SwingActiveRender) [![Github](https://badge.dorkbox.com/github.svg "Github")](https://github.com/dorkbox/SwingActiveRender) [![Gitlab](https://badge.dorkbox.com/gitlab.svg "Gitlab")](https://gitlab.com/dorkbox/SwingActiveRender)
+
+
+The files here are copyright by many different authors, usually Apache 2.0, MIT, or BSD. 
+
+Please see the header of each file for the specific license that applies to it. Specific license details are in the LICENSE file.
+
+
+
+Maven Info
+---------
+```
+<dependencies>
+    ...
+    <dependency>
+      <groupId>com.dorkbox</groupId>
+      <artifactId>SwingActiveRender</artifactId>
+      <version>1.0</version>
+    </dependency>
+</dependencies>
+```
+
+Gradle Info
+---------
+````
+dependencies {
+    ...
+    compile "com.dorkbox:SwingActiveRender:1.0"
+}
+````
+
+
+License
+---------
+This project is © 2021 dorkbox llc, and is distributed under the terms of the Apache v2.0 License. See file "LICENSE" for further 
+references.
+
---
+++ build.gradle.kts
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2021 dorkbox, llc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.time.Instant
+
+///////////////////////////////
+//////    PUBLISH TO SONATYPE / MAVEN CENTRAL
+////// TESTING : (to local maven repo) <'publish and release' - 'publishToMavenLocal'>
+////// RELEASE : (to sonatype/maven central), <'publish and release' - 'publishToSonatypeAndRelease'>
+///////////////////////////////
+
+gradle.startParameter.showStacktrace = ShowStacktrace.ALWAYS   // always show the stacktrace!
+gradle.startParameter.warningMode = WarningMode.All
+
+plugins {
+    java
+
+    id("com.dorkbox.GradleUtils") version "1.12"
+    id("com.dorkbox.Licensing") version "2.5.2"
+    id("com.dorkbox.VersionUpdate") version "2.1"
+    id("com.dorkbox.GradlePublish") version "1.8"
+}
+
+object Extras {
+    // set for the project
+    const val description = "Active Renderer for Swing"
+    const val group = "com.dorkbox"
+    const val version = "1.0"
+
+    // set as project.ext
+    const val name = "SwingActiveRender"
+    const val id = "SwingActiveRender"
+    const val vendor = "Dorkbox LLC"
+    const val vendorUrl = "https://dorkbox.com"
+    const val url = "https://git.dorkbox.com/dorkbox/SwingActiveRender"
+
+    val buildDate = Instant.now().toString()
+}
+
+///////////////////////////////
+/////  assign 'Extras'
+///////////////////////////////
+GradleUtils.load("$projectDir/../../gradle.properties", Extras)
+GradleUtils.fixIntellijPaths()
+GradleUtils.defaultResolutionStrategy()
+GradleUtils.compileConfiguration(JavaVersion.VERSION_1_6)
+
+
+licensing {
+    license(License.APACHE_2) {
+        description(Extras.description)
+        author(Extras.vendor)
+        url(Extras.url)
+    }
+}
+
+sourceSets {
+    main {
+        java {
+            setSrcDirs(listOf("src"))
+
+            // want to include java files for the source. 'setSrcDirs' resets includes...
+            include("**/*.java")
+        }
+    }
+}
+
+repositories {
+    mavenLocal() // this must be first!
+    jcenter()
+}
+
+tasks.jar.get().apply {
+    manifest {
+        // https://docs.oracle.com/javase/tutorial/deployment/jar/packageman.html
+        attributes["Name"] = Extras.name
+
+        attributes["Specification-Title"] = Extras.name
+        attributes["Specification-Version"] = Extras.version
+        attributes["Specification-Vendor"] = Extras.vendor
+
+        attributes["Implementation-Title"] = "${Extras.group}.${Extras.id}"
+        attributes["Implementation-Version"] = Extras.buildDate
+        attributes["Implementation-Vendor"] = Extras.vendor
+
+        attributes["Automatic-Module-Name"] = Extras.id
+    }
+}
+
+dependencies {
+    implementation("com.dorkbox:Utilities:1.5.1")
+}
+
+publishToSonatype {
+    groupId = Extras.group
+    artifactId = Extras.id
+    version = Extras.version
+
+    name = Extras.name
+    description = Extras.description
+    url = Extras.url
+
+    vendor = Extras.vendor
+    vendorUrl = Extras.vendorUrl
+
+    issueManagement {
+        url = "${Extras.url}/issues"
+        nickname = "Gitea Issues"
+    }
+
+    developer {
+        id = "dorkbox"
+        name = Extras.vendor
+        email = "email@dorkbox.com"
+    }
+}
