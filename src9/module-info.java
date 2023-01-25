3089bc0499dbec0baf0d5e4e1ee756ab90d4b565
Robinson <robinson@dorkbox.com>
2023-01-25 02:23:37 +0100

    updated module-info.java

--- src9/module-info.java
+++ src9/module-info.java
@@ -1,3 +1,8 @@
 module com.dorkbox.swingActiveRender {
     exports dorkbox.swingActiveRender;
+
+    requires transitive dorkbox.updates;
+    requires transitive com.dorkbox.propertyLoader;
+
+    requires transitive kotlin.stdlib;
 }
